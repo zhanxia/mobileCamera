4ed327f16932632c1e7374d27f13bc520ac6213d
jiazhanxia <jiazhanxia@beisen.com>
2016-09-30 10:54:10 +0800

    add js

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(function(){
+	var but = document.getElementById("getCamera");
+	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
+	but.onclick = function(){
+		navigator.getUserMedia({
+			video:true
+		},function(stream){
+			successCallback(stream);
+		},function(){
+			window.isRejectGamera=true;
+			// Talent.app.execute('history:navigate', '#quiz/login-error?type=camera', true);
+		})
+	}
+
+	function successCallback(stream){
+		var video = document.createElement('video');
+		video.height=150;
+		$('.qu_img').html(video).show();
+		video.src = (window.URL&&window.URL.createObjectURL(stream))||stream; 
+		if(video.paused==true){
+			video.play();
+		}
+	}
+})
